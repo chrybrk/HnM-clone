38fe6ded81d5a48dbb9cbe57392946431321c5f0
chrybrk <rahul003chry@gmai.com>
2024-05-03 04:11:23 +0530

    new arrivals

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,18 @@
 var bt = document.getElementsByClassName("sh-btn");
+var photo = document.getElementsByClassName("photo");
 
 for (let i = 0; i < bt.length; i++) {
     bt[i].addEventListener("click", function (event) {
-        for (let j = 0; j < bt.length; j++) bt[j].classList.remove("sh-btn-active");
+        for (let j = 0; j < bt.length; j++) {
+            bt[j].classList.remove("sh-btn-active");
+
+            if (event.target.textContent === bt[j].textContent) {
+                for (let k = 0; k < photo.length; k++)
+                    photo[k].classList.add("sc-hide");
+                photo[j].classList.remove("sc-hide");
+            }
+        }
+
         event.target.classList.add("sh-btn-active");
     });
 }
