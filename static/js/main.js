a5cc97b7a66230bfd7bb994da94985ffa9a8c58c
chrybrk <rahul003chry@gmai.com>
2024-05-03 05:41:59 +0530

    finalizingg

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,27 @@
 var bt = document.getElementsByClassName("sh-btn");
 var photo = document.getElementsByClassName("photo");
 
+var da = document.getElementById("das");
+document.addEventListener("mousedown", function(event){
+    var e= document.getElementById("das_menu");
+    if (da.contains(event.target)) {
+        if (e.style.display === "none") e.style.display = "block";
+        else e.style.display = "none";
+    } else {
+        e.style.display = "none";
+    }
+});
+
+var sim = document.getElementById("sim");
+document.addEventListener("mouseover", function(event) {
+    var e = document.getElementById("sign_in_menu");
+    if (sim.contains(event.target) || e.contains(event.target)) {
+        e.style.display = "block";
+    } else {
+        e.style.display = "none";
+    }
+});
+
 for (let i = 0; i < bt.length; i++) {
     bt[i].addEventListener("click", function (event) {
         for (let j = 0; j < bt.length; j++) {
